88169960df7c398e3e3b35927aadddb42b1e85a5
vanitysys28 <guiaubzer@gmail.com>
2024-05-05 23:58:48 +0200

    script without psk updating capability

---
+++ script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "Scan for nearby networks? (y/n)"
+read scan
+
+if [ $scan = "y" ]; then
+    wpa_cli scan > /dev/null;
+    wpa_cli scan_results
+fi
+
+echo "Please enter the network SSID:"
+read SSID
+
+if grep -w $SSID'"' /etc/wpa_supplicant/wpa_supplicant.conf > /dev/null; then 
+    echo "SSID already saved in the configuration. Do you want to update the network password? (y/n)"
+    read update
+    
+    if [ "$update" == "n" ]; then
+	exit
+    fi
+
+    # Delete block associated with SSID
+fi
+
+echo "Please enter the network password:"
+read password
+
+echo "Encrypt network password? (y/n)"
+read encrypt
+
+if [ $encrypt = "y" ]; then
+    wpa_passphrase $SSID $password | tee -a /etc/wpa_supplicant/wpa_supplicant.conf > /dev/null
+    exit
+fi
+
+echo "network={" >> /etc/wpa_supplicant/wpa_supplicant.conf
+echo -e "\tssid=\"$SSID\"" >> /etc/wpa_supplicant/wpa_supplicant.conf
+echo -e "\tpsk=\"$password\"" >> /etc/wpa_supplicant/wpa_supplicant.conf
+echo -e "\tpriority=50" >> /etc/wpa_supplicant/wpa_supplicant.conf
+echo "}" >> /etc/wpa_supplicant/wpa_supplicant.conf
